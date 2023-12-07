6dc0c38de58e3eee65d6f1ab4a9dd73af6c406ad
MWEBESA JOHNSON <143801904+BISHOP122222@users.noreply.github.com>
2023-12-07 12:29:12 +0300

    Add files via upload

---
+++ work.js
@@ -0,0 +1,4 @@
+var name = window.prompt("whats your name?");
+var age = window.prompt("How old are you?");
+document.write("<hr/> ");
+document.write("Hey "+ name + ",How are you?"+ ' ' + age + " years old?");
